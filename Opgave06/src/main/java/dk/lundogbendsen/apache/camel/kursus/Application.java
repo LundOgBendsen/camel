4df816493ac57bb70154088468773bed01eba19a
Christian Damsgaard <christian@damsgaard.biz>
2015-12-15 21:33:27 +0100

    Implemented opgave 06.

---
+++ Opgave06/src/main/java/dk/lundogbendsen/apache/camel/kursus/Application.java
@@ -0,0 +1,28 @@
+package dk.lundogbendsen.apache.camel.kursus;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.ConnectionFactory;
+
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ActiveMQConnectionFactory jmsFactory() {
+        return new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(final ConnectionFactory connectionFactory) {
+        return new JmsTemplate(connectionFactory);
+    }
+}
---
+++ Opgave06/src/main/java/dk/lundogbendsen/apache/camel/kursus/MainRoute.java
@@ -0,0 +1,14 @@
+package dk.lundogbendsen.apache.camel.kursus;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MainRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("file:input")
+            .split(xpath("/prefix:orders/prefix:order").namespace("prefix", "http://www.lundogbendsen.dk/apache/camel/kursus"))
+            .to("jms:queue:orders");
+    }
+}
